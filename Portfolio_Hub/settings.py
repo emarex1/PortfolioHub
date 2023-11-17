2cd720b86e4b19436305a4fabce9f19a128aad4c
emarex1 <emmanuelawoh0@gmail.com>
2023-11-17 01:14:28 +0100

    made migrations

--- Portfolio_Hub/settings.py
+++ Portfolio_Hub/settings.py
@@ -26,6 +26,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myinfo.apps.MyinfoConfig',
+    'projects.apps.ProjectsConfig',
+    'blogs.apps.BlogsConfig',
 ]
 
 MIDDLEWARE = [
