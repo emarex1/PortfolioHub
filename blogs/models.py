b34ac66f23b8f78a1841b8b58553a21b9aeca795
emarex1 <emmanuelawoh0@gmail.com>
2023-12-12 00:45:39 +0100

    create Recent_post model

--- blogs/models.py
+++ blogs/models.py
@@ -24,3 +24,22 @@ class UserProfile(models.Model):
         if self.pics:
             return self.pics.url
         return None
+
+
+class RecentPosts(models.Model):
+    class Meta:
+        ordering = ['-date']
+    title = models.CharField(max_length=50)
+    title_type = models.CharField(max_length=20)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE, null=True
+    )
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("recent_post_detail", kwargs={"pk": self.pk})
--- blogs/views.py
+++ blogs/views.py
@@ -5,3 +5,4 @@ from .models import UserProfile
 class UserProfileListView(ListView):
     model = UserProfile
     template_name = 'home.html'
+    
