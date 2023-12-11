e89ecb3e3824c904de2ba8a10ef36ab156e7f26f
emarex1 <emmanuelawoh0@gmail.com>
2023-12-11 22:46:53 +0100

    removed myinfo app

--- Portfolio_Hub/settings.py
+++ Portfolio_Hub/settings.py
@@ -27,7 +27,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myinfo.apps.MyinfoConfig',
-    'projects.apps.ProjectsConfig',
     'blogs.apps.BlogsConfig',
     "accounts.apps.AccountsConfig",
     "pages.apps.PagesConfig",
