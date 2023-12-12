a0e1ce85261f95fa9cffbebd012352e44b83cab2
emarex1 <emmanuelawoh0@gmail.com>
2023-12-12 01:03:14 +0100

    create recent_post view

--- blogs/views.py
+++ blogs/views.py
@@ -1,8 +1,24 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import UserProfile
 
 
 class UserProfileListView(ListView):
     model = UserProfile
     template_name = 'home.html'
-    
+
+class RecentPostsDetailView(DetailView):
+    model = RecentPosts
+    template_name = 'recent_posts_detail.html'
+    context_object_name = 'post'
+
+class RecentPostsLimitedView(ListView):
+    model = RecentPosts
+    template_name = 'recent_posts_list_limited.html'
+    context_object_name = 'posts'
+    queryset = RecentPosts.objects.all()[:2]  # Display only two posts
+
+class RecentPostsFullView(ListView):
+    model = RecentPosts
+    template_name = 'recent_posts_list_full.html'
+    context_object_name = 'posts'
+    queryset = RecentPosts.objects.all()  # Display all posts
